2bbe2e8b7574cc0a691136f45533e8df32707416
danielle3c <sofiafigueroa@liceovvh.cl>
2025-03-19 15:52:08 -0300

    ok

--- js/1.4 Condicionales en JS/script.js
+++ js/1.4 Condicionales en JS/script.js
@@ -13,16 +13,20 @@ function temperatura() {
         alert("esta agradable el clima");
     }
 
-function verificacion () {
-    let usuario = ("usaurio123");
-        let contraseña = ("secreto");
-        if (usuario == "usuario123 && contraseña == secreto"){
-            alert("acceso concedido")
-        } else { 
-            alert ("acceso denegado")
+    function verificacion() {
+        let usuario = "usuario123";
+        let contraseña = "secreto";
+    
+        let respuesta1 = prompt("Introduce tu usuario");
+        let respuesta2 = prompt("Introduce tu contraseña");
+    
+        if (usuario === respuesta1 && contraseña === respuesta2) {
+            alert("Acceso concedido");
+        } else {
+            alert("Acceso denegado");
         }
+    }
 
     
-    
-}
+
 
