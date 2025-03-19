71f218b5acc65d2b43abf72f066c62efa3cb5658
danielle3c <sofiafigueroa@liceovvh.cl>
2025-03-19 09:28:25 -0300

    ok

---
+++ js/1.4 Condicionales en JS/script.js
@@ -0,0 +1,28 @@
+console.log("Funcionando correctamente...");
+function temperatura() {
+    let temperatura = parseInt(prompt("introduce la temaperatura actual en grado celcius."));
+    if (temperatura <0) {
+        alert("hace mucho frio");
+    }
+    if (temperatura >=0 && temperatura <25) {
+        alert("esta agradable el clima");
+    } else {
+        alert ("hace mucho calor");
+    }
+    
+        alert("esta agradable el clima");
+    }
+
+function verificacion () {
+    let usuario = ("usaurio123");
+        let contraseña = ("secreto");
+        if (usuario == "usuario123 && contraseña == secreto"){
+            alert("acceso concedido")
+        } else { 
+            alert ("acceso denegado")
+        }
+
+    
+    
+}
+
