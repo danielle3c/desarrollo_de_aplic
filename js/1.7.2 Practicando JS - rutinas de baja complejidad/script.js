27b022cb994fe9b33b643be986d21cca2b9b68a1
danielle3c <sofiafigueroa@liceovvh.cl>
2025-03-31 11:14:21 -0300

    ok

--- js/1.7.2 Practicando JS - rutinas de baja complejidad/script.js
+++ js/1.7.2 Practicando JS - rutinas de baja complejidad/script.js
@@ -4,16 +4,15 @@ function tabla_multiplicar_personalizada() {
     let numero = parseInt(prompt("Introduce un número para la tabla de multiplicar"));
     let limite = parseInt(prompt("Introduce el límite de la tabla de multiplicar"));
     let resultados = [];
-    
+
     alert(`Tabla de multiplicar del ${numero} hasta el ${limite}:`);
-    for (let i = 1; i <= limite; i++) {
-        let resultado = numero * i;
-        resultados.push(`${numero} * ${i} = ${resultado}`);
+    for (let count = 1; count <= limite; count++) {
+        let resultado = numero * count;
+        resultados.push(`${numero} * ${count} = ${resultado}`);
     }
     alert(resultados.join("\n"));
     alert(`Total de resultados: ${resultados.length}`);
 }
-
 
 function suma_N_numeros_ingresados_por_usuarios() {
     let n = parseInt(prompt("¿Cuántos números deseas sumar?"));
@@ -32,18 +31,16 @@ function suma_N_numeros_ingresados_por_usuarios() {
 }
 
 function mostrar_numeros_pares_entre_dos_valores_dados() {
-    let valor1 = parseInt(prompt("introduce el primer valor"));
-    let valor2 = parseInt(prompt("introduce el segundo valor"));
+    let valor1 = parseInt(prompt("Introduce el primer valor"));
+    let valor2 = parseInt(prompt("Introduce el segundo valor"));
     let array = [];
-    for (let count = valor1 ; count <= valor2; count++){
+
+    for (let count = valor1; count <= valor2; count++) {
         if (count % 2 == 0) {
             array.push(count);
-        } 
+        }
     }
-    alert("los numeros son:" + array.join("-"));
-    alert("total de numeros pares:" + array.length);
-    
 
-
+    alert(`Los números son: ${array.join("-")}`);
+    alert(`Total de números pares: ${array.length}`);
 }
-
